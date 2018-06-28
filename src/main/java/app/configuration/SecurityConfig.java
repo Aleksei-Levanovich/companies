db3d15a57de32aba20db1b3566a9ef1764103d3a
Алексей Леванович <levanovich_aa@icloud.com>
2018-06-28 14:49:17 +0300

    Работа с сущностями
    добавлена шифровка пароля, несколько методов для Users

---
+++ src/main/java/app/configuration/SecurityConfig.java
@@ -0,0 +1,54 @@
+package app.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder)
+        throws Exception{
+
+        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select login, password from users where login=?")
+                .authoritiesByUsernameQuery("select user_id, role_id from user_role where user_id =?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login?error")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout")
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .and()
+                .csrf();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+}
--- src/main/java/app/entities/Users.java
+++ src/main/java/app/entities/Users.java
@@ -3,6 +3,8 @@ package app.entities;
 import javax.persistence.*;
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Entity
 @Table(name="users")
 public class Users {
@@ -35,4 +37,48 @@ public class Users {
     private List<Roles> roles;
 
     public Users() {}
+
+    //login
+    public String getLogin(){
+        return login;
+    }
+    public void setLogin(String login){
+        this.login=login;
+    }
+
+    //password
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        password = passwordEncoder.encode(password);
+        this.password = password;
+    }
+
+    //names
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
 }
